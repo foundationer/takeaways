2078a7ed6a4656ee733c8d45c365ed36c01b447f
foundationer <foundationer@tuta.io>
2018-01-17 19:50:34 -0300

    Created project skeleton with Leiningen.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to takeaways
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/takeaways/core_test.clj
@@ -0,0 +1,7 @@
+(ns takeaways.core-test
+  (:require [clojure.test :refer :all]
+            [takeaways.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
