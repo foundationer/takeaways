46f385055ee29f392a2ab872585d66a47de377d4
Abel Quiros <beluchin@gmail.com>
2018-02-06 20:47:57 -0300

    test for dups - dups are allowed

--- test/takeaways/services/test_data_access.clj
+++ test/takeaways/services/test_data_access.clj
@@ -12,3 +12,15 @@
                    (add-takeaway "the title")
                    (get-takeaways))
                  "the title")))
+
+(deftest dups
+  (let [title "t"
+        ts (do (add-takeaway title)
+               (add-takeaway title)
+               (filter #(= title %) (get-takeaways)))]
+    (is (<= 2 (count ts)))))
+
+
+;; currently add-takeaway returns the takeaways.
+;; that does not seem right. It is a behavior that's
+;; not required at the moment.
