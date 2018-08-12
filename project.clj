96d44637b0e07366b7c848e517da4f64e80feb28
Lucas Godoy <foundationer@tuta.io>
2018-08-12 18:07:02 -0300

    Fixed project configuration.

--- project.clj
+++ project.clj
@@ -3,10 +3,12 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [mount "0.1.11"]]
+  :dependencies [
+                 [org.clojure/clojure "1.9.0"]
+                 [mount "0.1.11"]
                  [midje "1.9.1"]
-                 [com.datomic/datomic-free "0.9.5656"]
+                 ;[com.datomic/datomic-free "0.9.5697"]
+                ]
   :main ^:skip-aot takeaways.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
